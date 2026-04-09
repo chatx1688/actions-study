52f2fbf299983b02e1aabe0c84ab9e915938879e
XX <xx@XXdeMacBook-Pro.local>
2026-04-09 15:39:37 +0800

    ttjs

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+
+const buildTime = import.meta.env.VITE_BUILD_TIME
+const buildAuthor = import.meta.env.VITE_BUILD_AUTHOR
 </script>
 
 <template>
   <HelloWorld />
+  <footer>
+    <p>最后构建于：{{ buildTime }}</p>
+    <p>构建人：{{ buildAuthor }}</p>
+  </footer>
 </template>
