fde9820c71300ec4b0fa9c132edf1f1f7c7678af
XX <xx@XXdeMacBook-Pro.local>
2026-04-09 15:35:25 +0800

    ttjj

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/github_action_study/', // 👈 就是这一行！填你的仓库名字
+  base: './', // 👈 就是这一行！填你的仓库名字
 })
